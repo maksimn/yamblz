9812cc4b0f3e0e490c28d34d9efa93fa4c532966
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2018-11-10 14:50:43 +0300

    Android Studio Project init

---
+++ yamblz-contentprovider-sqlite/app/src/main/java/maksimn/github/io/yamblzcontentprovidersqlite/MainActivity.java
@@ -0,0 +1,13 @@
+package maksimn.github.io.yamblzcontentprovidersqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ yamblz-contentprovider-sqlite/app/src/test/java/maksimn/github/io/yamblzcontentprovidersqlite/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package maksimn.github.io.yamblzcontentprovidersqlite;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
