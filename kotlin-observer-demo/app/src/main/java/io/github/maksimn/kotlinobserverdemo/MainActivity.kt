cfe7ecc351f37f1ef1f48f366755e3ec19f9e8fc
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2018-12-13 10:22:16 +0300

    observer demo init

--- kotlin-observer-demo/app/src/main/java/io/github/maksimn/kotlinobserverdemo/MainActivity.kt
+++ kotlin-observer-demo/app/src/main/java/io/github/maksimn/kotlinobserverdemo/MainActivity.kt
@@ -2,11 +2,77 @@ package io.github.maksimn.kotlinobserverdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.properties.Delegates
+
+data class NewMailEventArgs(val from: String, val to: String, val subject: String)
+
+interface NewMailListener {
+    fun onNewMail(newMail: NewMailEventArgs)
+}
+
+class MailManager {
+    private val newMailListeners = mutableListOf<NewMailListener>()
+
+    private var newMail: NewMailEventArgs by Delegates.observable(
+            NewMailEventArgs("", "", "")
+    ) { _, _, newMail->
+
+        Log.i("Mail Manager ", "is emitting a new mail")
+
+        for (listener in newMailListeners) {
+            listener.onNewMail(newMail)
+        }
+    }
+
+    fun addNewMailListener(listener: NewMailListener) {
+        newMailListeners.add(listener)
+    }
+
+    fun removeNewMailListener(listener: NewMailListener) {
+        newMailListeners.remove(listener)
+    }
+
+    fun simulateNewMail(newMail: NewMailEventArgs) {
+        this.newMail = newMail
+    }
+}
+
+class Fax : NewMailListener {
+    override fun onNewMail(newMail: NewMailEventArgs) {
+        Log.i("Fax received ", newMail.toString())
+    }
+}
+
+class Pager : NewMailListener {
+    override fun onNewMail(newMail: NewMailEventArgs) {
+        Log.i("Pager received ", newMail.toString())
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // DEMO CODE:
+
+        val mailManager = MailManager()
+        val fax = Fax()
+        val pager = Pager()
+
+        mailManager.addNewMailListener(fax)
+        mailManager.addNewMailListener(pager)
+
+        mailManager.simulateNewMail(NewMailEventArgs("Alice", "Bob", "SHUE"))
+
+        mailManager.removeNewMailListener(fax)
+
+        mailManager.simulateNewMail(NewMailEventArgs("Stalin", "Lenin", "PPSH SHPSH"))
+
+        mailManager.removeNewMailListener(pager)
+
+        mailManager.simulateNewMail(NewMailEventArgs("A", "B", "C"))
     }
 }
