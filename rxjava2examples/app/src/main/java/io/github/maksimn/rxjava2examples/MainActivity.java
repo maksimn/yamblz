2362396b4cb96c11a755b0a59798419cb529d26f
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2018-12-18 20:09:50 +0300

    rxjava2 first example

--- rxjava2examples/app/src/main/java/io/github/maksimn/rxjava2examples/MainActivity.java
+++ rxjava2examples/app/src/main/java/io/github/maksimn/rxjava2examples/MainActivity.java
@@ -2,6 +2,8 @@ package io.github.maksimn.rxjava2examples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import io.reactivex.Single;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Задача 1: создать "Observable", тип события - целое число, присвоить ему значение 42
+        // "подписаться" на это значение и вывести его в лог в обработчике
+        Single.just(42).subscribe(val -> Log.i("", val.toString()));
     }
 }
