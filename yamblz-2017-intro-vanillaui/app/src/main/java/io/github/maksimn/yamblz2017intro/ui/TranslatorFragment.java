ff345c8c0a3a1adf013b126a86cc787e7988faf3
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-02-26 03:31:19 +0300

    [refactor] SpinnerUtil class

--- yamblz-2017-intro-vanillaui/app/src/main/java/io/github/maksimn/yamblz2017intro/ui/TranslatorFragment.java
+++ yamblz-2017-intro-vanillaui/app/src/main/java/io/github/maksimn/yamblz2017intro/ui/TranslatorFragment.java
@@ -7,13 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
 import io.github.maksimn.yamblz2017intro.R;
 import io.github.maksimn.yamblz2017intro.data.repository.LangRepository;
-import io.github.maksimn.yamblz2017intro.util.Action;
+import io.github.maksimn.yamblz2017intro.util.SpinnerUtil;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
@@ -24,10 +21,21 @@ public class TranslatorFragment extends Fragment {
 
     private Disposable disposable;
 
+    private Spinner fromLangSpinner;
+    private Spinner toLangSpinner;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_translator, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fromLangSpinner = getActivity().findViewById(R.id.from_language_spinner);
+        toLangSpinner = getActivity().findViewById(R.id.to_language_spinner);
     }
 
     @Override
@@ -36,10 +44,8 @@ public class TranslatorFragment extends Fragment {
 
         langRepository = new LangRepository(null);
 
-        initializeSpinner(R.id.from_language_spinner,
-                langRepository.getLanguageNames(),
-                langRepository.defaultLanguage(),
-                this::fetchSupportedLanguages);
+        SpinnerUtil.setDataAndBehavior(fromLangSpinner, langRepository.getLanguageNames(),
+                langRepository.defaultLanguage(), this::fetchSupportedLanguages);
     }
 
     @Override
@@ -51,34 +57,6 @@ public class TranslatorFragment extends Fragment {
         }
     }
 
-    private void initializeSpinner(int spinnerId, String[] langs, String selectedLang,
-                                   Action<String> onItemSelectedCallback) {
-        final Spinner spinner = getActivity().findViewById(spinnerId);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
-                R.layout.item_language, langs);
-        final int langPos = adapter.getPosition(selectedLang);
-
-        spinner.setAdapter(adapter);
-        spinner.setSelection(langPos);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                final TextView spinnerTextView = (TextView) view;
-
-                if (spinnerTextView != null) {
-                    final String languageName = spinnerTextView.getText().toString();
-
-                    if (onItemSelectedCallback != null) {
-                        onItemSelectedCallback.run(languageName);
-                    }
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {}
-        });
-    }
-
     private void fetchSupportedLanguages(String selectedLang) {
         disposable = langRepository.getSupportedLanguageNames(selectedLang)
                 .subscribeOn(Schedulers.io())
@@ -87,11 +65,11 @@ public class TranslatorFragment extends Fragment {
     }
 
     private void fetchSupportedLanguagesSuccess(String[] supportedLangs) {
-        initializeSpinner(R.id.to_language_spinner, supportedLangs, supportedLangs[0], null);
+        SpinnerUtil.setDataAndBehavior(toLangSpinner, supportedLangs, supportedLangs[0], null);
     }
 
     private void fetchSupportedLanguagesError(Throwable e) {
         final String[] error = {"[Нет языка]"};
-        initializeSpinner(R.id.to_language_spinner, error, error[0], val -> {});
+        SpinnerUtil.setDataAndBehavior(toLangSpinner, error, error[0], null);
     }
 }
---
+++ yamblz-2017-intro-vanillaui/app/src/main/java/io/github/maksimn/yamblz2017intro/util/SpinnerUtil.java
@@ -0,0 +1,37 @@
+package io.github.maksimn.yamblz2017intro.util;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import io.github.maksimn.yamblz2017intro.R;
+
+public class SpinnerUtil {
+
+    public static void setDataAndBehavior(Spinner spinner, String[] langs, String selectedLang,
+                                          Action<String> onItemSelectedCallback) {
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(spinner.getContext(),
+                R.layout.item_language, langs);
+        final int langPos = adapter.getPosition(selectedLang);
+
+        spinner.setAdapter(adapter);
+        spinner.setSelection(langPos);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                final TextView spinnerTextView = (TextView) view;
+
+                if (spinnerTextView != null) {
+                    final String languageName = spinnerTextView.getText().toString();
+
+                    if (onItemSelectedCallback != null) {
+                        onItemSelectedCallback.run(languageName);
+                    }
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+    }
+}
