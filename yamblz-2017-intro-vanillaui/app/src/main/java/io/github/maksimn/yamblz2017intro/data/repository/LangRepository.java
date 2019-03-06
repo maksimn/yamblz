e20f31ee9ad3692f4a1ce7d9edc6f78b7d2db9b5
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-03-07 01:56:33 +0300

    [refactor] default langs to resource file

--- yamblz-2017-intro-vanillaui/app/src/main/java/io/github/maksimn/yamblz2017intro/data/repository/LangRepository.java
+++ yamblz-2017-intro-vanillaui/app/src/main/java/io/github/maksimn/yamblz2017intro/data/repository/LangRepository.java
@@ -1,6 +1,7 @@
 package io.github.maksimn.yamblz2017intro.data.repository;
 
 import android.content.Context;
+import android.content.res.Resources;
 import io.github.maksimn.yamblz2017intro.R;
 import io.github.maksimn.yamblz2017intro.data.pojo.Lang;
 import io.github.maksimn.yamblz2017intro.util.JsonUtil;
@@ -9,35 +10,43 @@ import io.reactivex.Single;
 
 public class LangRepository {
 
-    private static Lang[] smLangs;
-    private static String[] smLanguageNames;
+    private static Lang[] _langs;
+    private static String[] _languageNames;
+
+    private static String _defaultLanguage;
+    private static String _secondDefaultLanguage;
 
     public LangRepository(Context context) {
-        if (smLangs == null) {
-            final String languagesJson =
-                    ResourceUtil.readRawAsString(context.getResources(), R.raw.languages);
+        if (_langs == null) {
+            /*
+             * 1. Get list of all languages from a raw resource
+             */
+            final Resources res = context.getResources();
+            final String languagesJson = ResourceUtil.readRawAsString(res, R.raw.languages);
 
-            smLangs = JsonUtil.toLanguageList(languagesJson);
+            _langs = JsonUtil.toLanguageList(languagesJson);
 
-            final int n = smLangs.length;
+            final int n = _langs.length;
 
-            smLanguageNames = new String[n];
+            _languageNames = new String[n];
 
             for(int i = 0; i < n; i++) {
-                smLanguageNames[i] = smLangs[i].lang_name;
+                _languageNames[i] = _langs[i].lang_name;
             }
+
+            /*
+             * 2. Get default languages from app resources
+             */
+            _defaultLanguage = res.getString(R.string.default_language);
+            _secondDefaultLanguage = res.getString(R.string.second_default_language);
         }
     }
 
-    public Lang[] getLanguages() {
-        return smLangs;
-    }
+    public String[] getLanguageNames() { return _languageNames; }
 
-    public String[] getLanguageNames() { return smLanguageNames; }
+    public String defaultLanguage() { return _defaultLanguage; }
 
-    public String defaultLanguage() { return "Английский"; }
-
-    public String secondDefaultLanguage() { return "Русский"; }
+    public String secondDefaultLanguage() { return _secondDefaultLanguage; }
 
     public Single<String[]> getSupportedLanguageNames(String langName) {
         if ("Английский".equals(langName)) {
